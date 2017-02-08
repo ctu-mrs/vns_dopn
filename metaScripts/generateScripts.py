501070bdc041f45be2cb61d2b7e63cf8d3fdbad8
Robert Pěnička <penicrob@fel.cvut.cz>
2017-02-08 17:03:52 +0100

    working on scripting some datasets

--- metaScripts/generateScripts.py
+++ metaScripts/generateScripts.py
@@ -5,12 +5,21 @@ import re
 from __builtin__ import exit
 from shutil import copyfile
 
-#SCRIPT_OUTPUT_FOLDER = "generatedScripts/"
-SCRIPT_OUTPUT_FOLDER = "/storage/praha1/home/penicrob/generatedScripts/"
+#for my PC
+SCRIPT_OUTPUT_FOLDER = "generatedScripts/"
+SCRIPT_START_FOLDER = "scriptStartFolder/"
+
+#for metacenter
+#SCRIPT_OUTPUT_FOLDER = "/storage/praha1/home/penicrob/generatedScripts/"
+#SCRIPT_START_FOLDER = "/storage/praha1/home/penicrob/scriptStartFolder/"
+
 PROBLEMS_PATH = "../"
 
 if not os.path.exists(SCRIPT_OUTPUT_FOLDER):
     os.makedirs(SCRIPT_OUTPUT_FOLDER)
+
+if not os.path.exists(SCRIPT_START_FOLDER):
+    os.makedirs(SCRIPT_START_FOLDER)
 
 scriptId = 0
 
@@ -20,10 +29,17 @@ filelist = os.listdir(SCRIPT_OUTPUT_FOLDER)
 for oldFilename in filelist:
     print("remove " + oldFilename)
     os.remove(SCRIPT_OUTPUT_FOLDER + oldFilename)
+    
+print "remove all from folder SCRIPT_START_FOLDER"
+print os.listdir(SCRIPT_START_FOLDER)
+filelist = os.listdir(SCRIPT_START_FOLDER)
+for oldFilename in filelist:
+    print("remove " + oldFilename)
+    os.remove(SCRIPT_START_FOLDER + oldFilename)
 
 #copy start script
 START_SCRIPT_NAME = "startAllScripts.py"
-copyfile(START_SCRIPT_NAME, SCRIPT_OUTPUT_FOLDER+START_SCRIPT_NAME)
+copyfile(START_SCRIPT_NAME, SCRIPT_START_FOLDER+START_SCRIPT_NAME)
 
 
 templateName = "./template.sh"
@@ -43,10 +59,10 @@ NUM_ITERS_UNIMPR = 5000
 NUM_ITERS = 10000
 RVNS = 1
 dict = {'NAME':'noname', 'RESOLUTION':RESOLUTION, 'MAX_COMP_TIME_MIN':MAX_COMP_TIME_MIN, 'NUM_ITERS_UNIMPR': NUM_ITERS_UNIMPR , 'NUM_ITERS':NUM_ITERS, 'NEIGHBORHOOD_RESOLUTION':NEIGHBORHOOD_RESOLUTION, 'NEIGHBORHOOD_RADIUS': '0.3', 'RVNS':RVNS, 'RADIUS':'1.3', 'PROBLEM':'etc/tsiligrides_problem1/tsiligirides_problem_1_budget_20.txt', 'OUTPUT_FOLDER':'out'}
-#PROBLEM_NAMES = {'P1':'Tsiligirides Set 1', 'P2':'Tsiligirides Set 2', 'P3':'Tsiligirides Set 3', '64':'Chao diamond-shaped Set', '66':'Chao squared-shaped Set'}
 
 PROBLEM_FOLDERS = {'P1':'etc/tsiligrides_problem1/', 'P2':'etc/tsiligrides_problem2/', 'P3':'etc/tsiligrides_problem3/', '64':'etc/chao_set64-diamond/', '66':'etc/chao_set66-square/'}
 
+#PROBLEM_NAMES = {'P1':'Tsiligirides Set 1', 'P2':'Tsiligirides Set 2', 'P3':'Tsiligirides Set 3', '64':'Chao diamond-shaped Set', '66':'Chao squared-shaped Set'}
 PROBLEM_NAMES = {'66':'Chao squared-shaped Set'}
 
 templateFile = open(templateName, "r") 
--- metaScripts/startAllScripts.py
+++ metaScripts/startAllScripts.py
@@ -1,17 +1,56 @@
-#!/usr/bin/python3
-
 import os
+from string import Template
 import subprocess
+import re
+from __builtin__ import exit
+from shutil import copyfile
 
+#for my PC
 SCRIPT_OUTPUT_FOLDER = "generatedScripts/"
-SCRIPT_OUTPUT_FOLDER = "/storage/praha1/home/penicrob/generatedScripts/"
 
-scriptnames = next(os.walk(SCRIPT_OUTPUT_FOLDER))[2]
-print("test")
-for script in scriptnames:
-    fullname = SCRIPT_OUTPUT_FOLDER+script
-    #print(fullname)
-    subprocess.call(["ls",fullname])
-    subprocess.call(["qsub",fullname])
-    
-#subprocess.call(["ls", "-l",folderOut])
+#for metacenter
+#SCRIPT_OUTPUT_FOLDER = "/storage/praha1/home/penicrob/generatedScripts/"
+
+PROBLEMS_PATH = "../../"
+
+radiueses = [0.0 , 0.3 , 0.5 , 0.9 , 1.1 , 1.3 ]
+# radiueses = [0.0]
+
+neigh_radiuses = [0.0 , 0.2 , 0.5 , 1.0 , 1.5 , 2.0]
+testIds = range(0, 10)
+
+RESOLUTION = 16
+NEIGHBORHOOD_RESOLUTION = 16
+MAX_COMP_TIME_MIN = 110
+NUM_ITERS_UNIMPR = 5000
+NUM_ITERS = 10000
+RVNS = 1
+dict = {'NAME':'noname', 'RESOLUTION':RESOLUTION, 'MAX_COMP_TIME_MIN':MAX_COMP_TIME_MIN, 'NUM_ITERS_UNIMPR': NUM_ITERS_UNIMPR , 'NUM_ITERS':NUM_ITERS, 'NEIGHBORHOOD_RESOLUTION':NEIGHBORHOOD_RESOLUTION, 'NEIGHBORHOOD_RADIUS': '0.3', 'RVNS':RVNS, 'RADIUS':'1.3', 'PROBLEM':'etc/tsiligrides_problem1/tsiligirides_problem_1_budget_20.txt', 'OUTPUT_FOLDER':'out'}
+
+PROBLEM_FOLDERS = {'P1':'etc/tsiligrides_problem1/', 'P2':'etc/tsiligrides_problem2/', 'P3':'etc/tsiligrides_problem3/', '64':'etc/chao_set64-diamond/', '66':'etc/chao_set66-square/'}
+
+#PROBLEM_NAMES = {'P1':'Tsiligirides Set 1', 'P2':'Tsiligirides Set 2', 'P3':'Tsiligirides Set 3', '64':'Chao diamond-shaped Set', '66':'Chao squared-shaped Set'}
+PROBLEM_NAMES = {'66':'Chao squared-shaped Set'}
+
+
+for NEIGHBORHOOD_RADIUS in neigh_radiuses:
+    for RADIUS in radiueses:
+        for TEST_ID in testIds :
+            for problem_key in PROBLEM_NAMES.iterkeys():
+                problem_path = PROBLEM_FOLDERS[problem_key]
+                problem_name = PROBLEM_NAMES[problem_key]
+                print problem_path
+                filenames = next(os.walk(PROBLEMS_PATH + problem_path))[2]
+                for problem_filename in filenames:
+                    problem_with_path = problem_path + problem_filename
+                    budget = 0;
+                    with open(PROBLEMS_PATH + problem_with_path) as f:
+                        for line in f:
+                            s = re.search(r'\d+', line)
+                            if s:
+                                budget = s.group().zfill(3)
+                                break
+                        scriptName = SCRIPT_OUTPUT_FOLDER + "start_r_" + str(RADIUS) + "_nr_" + str(NEIGHBORHOOD_RADIUS) + "_p_" + problem_key + "_b_" + budget + "_run_" + str(TEST_ID) + ".bash"
+                        print("starting",SCRIPT_OUTPUT_FOLDER+scriptName)
+                        #subprocess.call(["ls",fullname])
+                        #subprocess.call(["qsub",fullname])
