07a65e1118d9798a79ab49657917194505c1240f
Robert Penicka <penicrob@jessietpl.cesnet.cz>
2017-02-18 19:49:33 +0100

    minor changes in template and meta scripts

--- metaScripts/startAllScriptsProblemsP36466.py
+++ metaScripts/startAllScriptsProblemsP36466.py
@@ -10,12 +10,12 @@ import os.path
 REAL_RUN = False
 
 #for my PC
-SCRIPT_OUTPUT_FOLDER = "../generatedScripts/"
-PROBLEMS_PATH = "../../"
+#SCRIPT_OUTPUT_FOLDER = "../generatedScripts/"
+#PROBLEMS_PATH = "../../"
 
 #for metacenter
-#SCRIPT_OUTPUT_FOLDER = "/storage/praha1/home/penicrob/generatedScripts/"
-#PROBLEMS_PATH = "/storage/praha1/home/penicrob/gacr16/vns_dopn/"
+SCRIPT_OUTPUT_FOLDER = "/storage/praha1/home/penicrob/generatedScripts/"
+PROBLEMS_PATH = "/storage/praha1/home/penicrob/gacr16/vns_dopn/"
 
 
 #radiueses = [0.0 , 0.3 , 0.5 , 0.9 , 1.1 , 1.3 ]
